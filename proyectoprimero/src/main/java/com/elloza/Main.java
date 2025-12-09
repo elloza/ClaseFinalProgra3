fa15f8f8eb1f9ec384933f860426588f41e41424
El loza <elloza@users.noreply.github.com>
2025-12-09 17:06:55 +0100

    feat(workflow): add JavaDoc generation workflow
    - Implement GitHub Actions workflow for automatic JavaDoc generation.
    - Configure Maven project settings in pom.xml for Java 17.
    - Set up steps to clone repo, configure JDK, generate JavaDoc, and push to /docs.

---
+++ proyectoprimero/src/main/java/com/elloza/Main.java
@@ -0,0 +1,15 @@
+package com.elloza;
+
+
+/**
+ * Main class for the application.
+ */
+public class Main {
+    /***
+     * Main method to run the application.
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
